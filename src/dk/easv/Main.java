fecc45c1794a42b52df0e985d74e6a8a18540d8a
Henrik Boulund <>
2023-03-06 09:33:31 +0100

    Init

---
+++ src/dk/easv/Main.java
@@ -0,0 +1,11 @@
+package dk.easv;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Thread t = new HelloThread();
+        t.start();
+
+        System.out.println("Main thread has completed");
+    }
+}
